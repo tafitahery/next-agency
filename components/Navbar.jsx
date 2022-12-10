eb892d9daea0318202acc7310928c2d7e923d0a9
Tafita Hery <tafitahery@gmail.com>
2022-12-10 18:20:32 +0300

    Navbar Design

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,5 +1,28 @@
-import React from 'react';
+import styles from '../styles/Navbar.module.css';
+
+import Link from 'next/link';
 
 export default function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <div className={styles.container}>
+      <Link href="/">AVOCADO</Link>
+      <ul className={styles.list}>
+        <li className={styles.listItem}>
+          <Link href="/products/design">DESIGN</Link>
+        </li>
+        <li className={styles.listItem}>
+          <Link href="/products/development">DEVELOPMENT</Link>
+        </li>
+        <li className={styles.listItem}>
+          <Link href="/products/production">PRODUCTION</Link>
+        </li>
+        <li className={styles.listItem}>
+          <Link href="/products/photography">PHOTOGRAPHY</Link>
+        </li>
+        <li className={styles.listItem}>
+          <Link href="/products/contact">CONTACT</Link>
+        </li>
+      </ul>
+    </div>
+  );
 }
