e9925cb2061af71cce4eef70c81c074913e1d804
Tafita Hery <tafitahery@gmail.com>
2022-12-23 17:59:29 +0300

    Product page

--- pages/products/[name].js
+++ pages/products/[name].js
@@ -1,11 +1,25 @@
 import styles from '../../styles/Product.module.css';
 import { data } from '../../data';
+import Image from 'next/image';
+import Link from 'next/link';
 
 export default function Product({ product }) {
   return (
     <div className={styles.container}>
-      <div className={styles.cardL}></div>
-      <div className={styles.cardS}></div>
+      <div className={styles.cardL}>
+        {product.images.map((item) => (
+          <div key={item.id} className={styles.imgContainer}>
+            <Image src={item.url} style={{ objectFit: 'cover' }} fill alt="" />
+          </div>
+        ))}
+      </div>
+      <div className={styles.cardS}>
+        <h1 className={styles.title}>{product.title}</h1>
+        <p className={styles.desc}>{product.desc}</p>
+        <button className={styles.button}>
+          <Link href="/contact">Contact</Link>
+        </button>
+      </div>
     </div>
   );
 }
